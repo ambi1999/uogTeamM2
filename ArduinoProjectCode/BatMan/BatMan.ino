904a8f0e7d3ca8ac69aa61a41416d21b49f30620
jamo0ol <barho0om_95@hotmail.com>
2014-03-11 11:18:31 +0000

    First commint by Ibrahim and Amy

---
+++ ArduinoProjectCode/BatMan/BatMan.ino
@@ -0,0 +1,17 @@
+int notes [] = {293.66,329.66,349.23,392.00,440.00,493.88,523.25}
+
+
+void setup () {
+  Serial.begin (9600);
+}
+
+void loop () {
+  
+  int keyVal = analogRead(A0);
+  
+  Serial.println (keyVal);
+  
+  if(keyVal == 2023) {
+    tone (8, notes[0]);
+  }
+    
